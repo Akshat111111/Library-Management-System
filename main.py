cc170425fb91d861a98cdb62c1b69e16277e9a26
Akshat Sharma <111536616+Akshat111111@users.noreply.github.com>
2023-05-17 18:26:58 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -1,47 +1,74 @@
 # lib_oops.py
-I created a practice OOPs based code (Central Library) which I made using CWH videos.(File type:Python py file)
+
 class Library:
-    def __init__(self,listofbooks):
-        self.books = listofbooks
-        print("***Central Library***\n1)Showbooks\n2)Borrow Books\n3)Return Books\n4)Quit")
+    def __init__(self, books):
+        self._books = books
+        self._options = {
+            1: self._displayBooks,
+            2: self._issueBook,
+            3: self._returnBook,
+            4: self.quit,
+            5: self.addBook,
+            6: self.removeBook
+        }
+        self._menu()
 
-    def showBooks(self):
-        
-        for items in self.books:
-            print(items)
-    
-    def borrowbooks(self,a):
-        if a in self.books:
-            print(f'{a} ,The book you wanted to issued,was issued')
-            self.books.remove(a)
+    def _menu(self):
+        print("***Central Library***")
+        print("1) Show books")
+        print("2) Borrow books")
+        print("3) Return books")
+        print("4) Quit")
+        print("5) Add book")
+        print("6) Remove book")
+
+    def _displayBooks(self):
+        for book in self._books:
+            print(book)
+
+    def _issueBook(self):
+        book = input('Enter the name of the book you want to issue: ')
+        if book in self._books:
+            print(f'{book}, the book you wanted to issue, was issued')
+            self._books.remove(book)
         else:
-            print('Not there in library currently.Please check if font you entered matches exactly with Predefined books present in Library.')
+            print('The book is not available in the library.')
 
-    def returnbook(self,b):
-        print(f'Thank you for returning:{b}book')
-        self.books.append(b)
-    
+    def _returnBook(self):
+        book = input('Enter the name of the book you want to return: ')
+        print(f'Thank you for returning: {book} book')
+        self._books.append(book)
+
+    def addBook(self):
+        book = input('Enter the name of the book you want to add: ')
+        self._books.append(book)
+        print(f'{book} book has been added to the library.')
+
+    def removeBook(self):
+        book = input('Enter the name of the book you want to remove: ')
+        if book in self._books:
+            self._books.remove(book)
+            print(f'{book} book has been removed from the library.')
+        else:
+            print(f'{book} book is not available in the library.')
 
     def quit(self):
         return False
 
 
 if __name__ == "__main__":
-    while(True):
-        centralLibrary = Library(['Physics','Chemistry','Math','Biology'])
-        userinput = int(input("Enter a option :\n"))
+    books = ['Physics', 'Chemistry', 'Math', 'Biology']
+    centralLibrary = Library(books)
 
-        if userinput == 1:
-            centralLibrary.showBooks()
-        elif userinput == 2:
-            a = input('Enter name of book you want to issue:\n')
-            # try:
-            #     int(a)
-            # except Exception as e:
-            #     print(e)
-            centralLibrary.borrowbooks(a)
-        elif userinput == 3:
-            b = input('Enter name of book you want to return:\n')
-            centralLibrary.returnbook(b)
+    while True:
+        userinput = int(input("Enter an option: "))
+
+        if userinput in centralLibrary._options:
+            if userinput == 4:
+                if centralLibrary.quit():
+                    break
+            else:
+                centralLibrary._options[userinput]()
         else:
-            centralLibrary.quit()
+            print('Invalid input, please try again.')
+
